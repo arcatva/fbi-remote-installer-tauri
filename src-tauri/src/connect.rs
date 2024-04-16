c523710afeebe1797a4dc682e775f90f6ff6ce04
Zhefu ZHANG <a723356@gmail.com>
2024-04-17 01:16:40 +1200

    feat: change buffer struct implementation

--- src-tauri/src/connect.rs
+++ src-tauri/src/connect.rs
@@ -1,8 +1,11 @@
+use std::io::Bytes;
+
+use bytes::BytesMut;
 use tauri::Window;
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::join;
 use tokio::net::TcpStream;
-
 pub async fn connect_tcp(addr: String) -> Result<String, String> {
     match TcpStream::connect(addr).await {
         Ok(_) => Ok("Connected to 3DS".to_string()),
@@ -11,22 +14,23 @@ pub async fn connect_tcp(addr: String) -> Result<String, String> {
 }
 
 pub async fn sendfile_tcp(addr: String, file_path: String) -> Result<String, String> {
-    let mut stream = TcpStream::connect(addr)
-        .await
-        .map_err(|e| "Not connected".to_string())?;
+    let stream_future = TcpStream::connect(addr);
 
-    let mut file = File::open(file_path)
-        .await
-        .map_err(|e| "Failed to open the file".to_string())?;
+    let file_future = File::open(file_path);
 
+    let (stream_result, file_result) = join!(stream_future, file_future);
+    let (mut stream, mut file) = (
+        stream_result.map_err(|e| e.to_string())?,
+        file_result.map_err(|e| e.to_string())?,
+    );
     let total_size = file
         .metadata()
         .await
-        .map_err(|e| "Failed to open the file".to_string())?
+        .map_err(|_| "Failed to open the file".to_string())?
         .len();
     let mut total_read: u64 = 0;
 
-    let mut buffer = vec![0; 4096];
+    let mut buffer = BytesMut::with_capacity(1024);
     loop {
         let n = file
             .read_buf(&mut buffer)
@@ -36,7 +40,7 @@ pub async fn sendfile_tcp(addr: String, file_path: String) -> Result<String, Str
             break;
         }
         stream
-            .write_all(&buffer[..n])
+            .write_all(&buffer[..])
             .await
             .map_err(|e| e.to_string())?;
         total_read += n as u64;

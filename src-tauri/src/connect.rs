1c7e15b6206b3084d23eccab7971a4ed493218fd
Zhefu ZHANG <a723356@gmail.com>
2024-04-25 22:40:32 +1200

    feat: introduce hyper for setting up http file server

--- src-tauri/src/connect.rs
+++ src-tauri/src/connect.rs
@@ -1,53 +1,52 @@
-use std::io::Bytes;
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-use bytes::BytesMut;
-use tauri::Window;
-use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::join;
-use tokio::net::TcpStream;
+use http_body_util::Full;
+use hyper::{Request, Response};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
+use tokio::net::{TcpListener, TcpStream};
+
 pub async fn connect_tcp(addr: String) -> Result<String, String> {
-    match TcpStream::connect(addr).await {
-        Ok(_) => Ok("Connected to 3DS".to_string()),
-        Err(_) => Err("Not connected".to_string()),
-    }
+	match TcpStream::connect(addr).await {
+		Ok(_) => Ok("Connected to 3DS".to_string()),
+		Err(_) => Err("Not connected".to_string()),
+	}
 }
 
-pub async fn sendfile_tcp(addr: String, file_path: String) -> Result<String, String> {
-    let stream_future = TcpStream::connect(addr);
 
-    let file_future = File::open(file_path);
-
-    let (stream_result, file_result) = join!(stream_future, file_future);
-    let (mut stream, mut file) = (
-        stream_result.map_err(|e| e.to_string())?,
-        file_result.map_err(|e| e.to_string())?,
-    );
-    let total_size = file
-        .metadata()
-        .await
-        .map_err(|_| "Failed to open the file".to_string())?
-        .len();
-    let mut total_read: u64 = 0;
-
-    let mut buffer = BytesMut::with_capacity(1024);
-    loop {
-        let n = file
-            .read_buf(&mut buffer)
-            .await
-            .map_err(|e| e.to_string())?;
-        if n == 0 {
-            break;
-        }
-        stream
-            .write_all(&buffer[..n])
-            .await
-            .map_err(|e| e.to_string())?;
-        total_read += n as u64;
-        let progress = (total_read as f64 / total_size as f64) * 100.0;
-
-        // Window::emit("progress", progress.to_string()).map_err(|e| e.to_string())?;
-    }
-
-    Ok("Installed".to_string())
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+	Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
+
+
+pub async fn sendlink_tcp(addr: String, file_path: String) -> Result<String, String> {
+	//let stream = TcpStream::connect(addr).await.map_err(|e| { e.to_string() })?;
+	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+	let listener = TcpListener::bind(addr).await.map_err(|e| { e.to_string() })?;
+	let (s, _) = listener.accept().await.map_err(|e| { e.to_string() })?;
+	let io = TokioIo::new(s);
+	tokio::task::spawn(async move {
+		// Finally, we bind the incoming connection to our `hello` service
+		if let Err(err) = http1::Builder::new()
+			// `service_fn` converts our function in a `Service`
+			.serve_connection(io, service_fn(hello))
+			.await
+		{
+			eprintln!("Error serving connection: {:?}", err);
+		}
+	});
+	Ok("Installed".to_string())
+}
+
+
+
+
+
+
+
+
+
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,31 +3,37 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+use hyper_util::rt::TokioExecutor;
+use hyper_util::server::conn::auto;
+
 mod connect;
 mod file;
 
 #[tauri::command(async)]
 async fn list_files() -> Result<Vec<file::FileMeta>, ()> {
-    Ok(file::list_files(".").await.unwrap())
+	Ok(file::list_files(".").await.unwrap())
 }
 
 #[tauri::command(async)]
 async fn connect_tcp(addr: String) -> Result<String, String> {
-    Ok(connect::connect_tcp(addr).await?)
+	Ok(connect::connect_tcp(addr).await?)
 }
 
 #[tauri::command(async)]
 async fn sendfile_tcp(addr: String, file_path: String) -> Result<String, String> {
-    Ok(connect::sendfile_tcp(addr, file_path).await?)
+	Ok(connect::sendfile_tcp(addr, file_path).await?)
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
+	auto::Builder::new(TokioExecutor::new());
+	
+
+	tauri::Builder::default()
+		.invoke_handler(tauri::generate_handler![
             list_files,
             connect_tcp,
             sendfile_tcp
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
